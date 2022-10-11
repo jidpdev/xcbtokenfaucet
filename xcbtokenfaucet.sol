4dc3d721e8111f1e28d35146075be2ca70e46b49
Jorge <46224205+jidpdev@users.noreply.github.com>
2022-10-11 19:20:11 -0300

    Update xcbtokenfaucet.sol
    . Add pause contract function
    . Add a claim timelocker function
    . Code cleaning

--- xcbtokenfaucet.sol
+++ xcbtokenfaucet.sol
@@ -1,22 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-// Crypto Birds XCB Token Faucet
+// Crypto Birds XCB Token Faucet 1.1
 // This smart contract is created by the Community and is not affiliated with Crypto Birds Platform.
 // Please visit cryptobirds.com if you are looking for official information.
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
 
-contract FaucetTokenXCB {
+contract XCBRewards {
 
     address public owner;
+    bool public paused;
     IERC20Metadata public tokenContract;
-    uint256 public setRewardMin;
-    uint256 public setRewardMax;
-    uint256 public setTimeWait;    
-    uint256 public setAllowedClaims;
-    uint256 internal claimCount;
+    uint256 public rewardMin;
+    uint256 public rewardMax;
+    uint256 public rewardWaitingTime;    
+    uint256 public rewardAllowed;
+    uint256 internal rewardCount;
+    uint256 public rewardTimelock;
+    uint256 internal rewardLastClaimed;
 
     mapping(address => uint256) public rewardGains;
     mapping(address => uint256) public rewardLastTime;
@@ -24,18 +27,21 @@ contract FaucetTokenXCB {
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor (IERC20Metadata _tokenContract, 
-                uint256 _setRewardMin, 
-                uint256 _setRewardMax, 
-                uint256 _setTimeWait,
-                uint256 _setAllowedClaims
+                uint256 _rewardMin, 
+                uint256 _rewardMax, 
+                uint256 _rewardWaitingTime,
+                uint256 _rewardAllowed,
+                uint256 _rewardTimelock
         ) {
         owner = msg.sender;
         tokenContract = _tokenContract;
-        setRewardMin = _setRewardMin;
-        setRewardMax = _setRewardMax;
-        setTimeWait = _setTimeWait;
-        setAllowedClaims = _setAllowedClaims;
-        claimCount = 0;
+        rewardMin = _rewardMin;
+        rewardMax = _rewardMax;
+        rewardWaitingTime = _rewardWaitingTime;
+        rewardAllowed = _rewardAllowed;
+        rewardTimelock = _rewardTimelock;
+        rewardCount = 0;
+        rewardLastClaimed = 0;
     }
 
     modifier onlyOwner() {
@@ -46,85 +52,102 @@ contract FaucetTokenXCB {
     // ----------------------
     // Contract info
 
-    function contractBalance() external view returns (uint256) {
+    function getContractBalance() external view returns (uint256) {
         return tokenContract.balanceOf(address(this)); 
     }
 
+    function setContractPaused(bool _paused) public onlyOwner {
+        paused = _paused;
+    }
 
     // ----------------------
     // Token info
 
-    function tokenName() external view returns (string memory) {
+    function getTokenName() external view returns (string memory) {
         return tokenContract.name(); 
     }
 
-    function tokenSymbol() external view returns (string memory) {
+    function getTokenSymbol() external view returns (string memory) {
         return tokenContract.symbol(); 
     }
 
     // ----------------------
-    // Faucet  
+    // Rewards Center  
 
-    function faucetClaim() public {
-        require(claimCount < setAllowedClaims, "Error. Completed.");
-        require(tokenContract.balanceOf(address(this)) > setRewardMax, "Error. Insufficient balance.");
-        require(faucetWalletEnabled(),"Error. You must wait to receive rewards again.");
-		require(setRewardMax >= setRewardMin,"Error. Setting wrong.");
-        uint _amountReward = faucetAmountReward();
+    function claimReward() public {
+        require(paused == false, "Error. Contract paused");
+        require(rewardCount < rewardAllowed, "Error. Completed.");
+        require(tokenContract.balanceOf(address(this)) > rewardMax, "Error. Insufficient balance.");
+        require(isWalletEnabled(msg.sender),"Error. You must wait to receive rewards again.");
+        require(rewardMax >= rewardMin,"Error. Setting wrong.");
+        require(block.timestamp >= rewardLastClaimed + rewardTimelock, "Error. You must wait at Locker Time.");
+        uint _amountReward = setAmountReward();
         tokenContract.transfer(address(msg.sender), _amountReward);
         rewardGains[msg.sender] = _amountReward;
         rewardLastTime[msg.sender] = block.timestamp;
-        claimCount++;
+        rewardCount++;
+        rewardLastClaimed = block.timestamp;
         emit Transfer(address(this), msg.sender, _amountReward);
     } 
 
-     function faucetWalletEnabled() internal view returns (bool) {
-        if(rewardLastTime[msg.sender] == 0) {
+     function isWalletEnabled(address _user) internal view returns (bool) {
+        if(rewardLastTime[_user] == 0) {
             return true;
-        } else if(block.timestamp >= rewardLastTime[msg.sender] + setTimeWait) {
+        } else if(block.timestamp >= rewardLastTime[_user] + rewardWaitingTime) {
             return true;
         }
         return false;
     }
 
-    function faucetWalletTimer() public view returns (uint256) {
-		uint timer = 0;
-		if (faucetWalletEnabled() == false ) {
-			timer = rewardLastTime[msg.sender] + setTimeWait - block.timestamp;
-		}
+    function getWalletWaitingTime(address _user) public view returns (uint256) {
+        uint timer = 0;
+        if (isWalletEnabled(_user) == false ) {
+            timer = rewardLastTime[_user] + rewardWaitingTime - block.timestamp;
+        }
         return timer; 
     }
 
-    function faucetAmountReward() internal view returns(uint){
+    function setAmountReward() internal view returns(uint){
         uint amount;
-        if(setRewardMax > setRewardMin) {
-            uint number = setRewardMax - setRewardMin;
-            amount = setRewardMin + uint(blockhash(block.number-1)) % number;
+        if(rewardMax > rewardMin) {
+            uint number = rewardMax - rewardMin;
+            amount = rewardMin + uint(blockhash(block.number-1)) % number;
         } else {
-            amount = setRewardMax;
+            amount = rewardMax;
         }
         return amount;
     }   
 
-    function faucetSuccessClaims() public view returns (uint256) {
-        return claimCount; 
-    }
-
-    function faucetWithdraw(uint256 _tokenAmount) public onlyOwner {
-         tokenContract.transfer(address(msg.sender), _tokenAmount);
+    function getSuccessClaims() public view returns (uint256) {
+        return rewardCount; 
     }
     
-    function faucetUpdate(
-            uint256 _setRewardMin, 
-            uint256 _setRewardMax, 
-            uint256 _setTimeWait,
-            uint256 _setAllowedClaims
+    function getTimelockCountdown() public view returns (uint256) {
+        if ( block.timestamp >= rewardLastClaimed + rewardTimelock ) {
+            return 0;
+        } else {
+            return rewardLastClaimed + rewardTimelock - block.timestamp;
+        }
+    }    
+
+    function withdrawRewards(uint256 _tokenAmount) public onlyOwner {
+        tokenContract.transfer(address(msg.sender), _tokenAmount);
+    }
+    
+    function updateRewards(
+            uint256 _rewardMin, 
+            uint256 _rewardMax, 
+            uint256 _rewardWaitingTime,
+            uint256 _rewardAllowed,
+			uint256 _rewardTimelock
         ) public onlyOwner {
-        setRewardMin = _setRewardMin;
-        setRewardMax = _setRewardMax;
-        setTimeWait = _setTimeWait;
-        setAllowedClaims = _setAllowedClaims;
-        claimCount = 0;
+        rewardMin = _rewardMin;
+        rewardMax = _rewardMax;
+        rewardWaitingTime = _rewardWaitingTime;
+        rewardAllowed = _rewardAllowed;
+		rewardTimelock = _rewardTimelock;
+        rewardCount = 0;
+		rewardLastClaimed = 0;
     } 
 
 }
